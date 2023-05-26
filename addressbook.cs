16e54884a852bdf1d4a2a7767cba71adac0b4566
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-26 22:02:57 +0530

    Add: add new contact code is added

--- addressbook.cs
+++ addressbook.cs
@@ -8,14 +8,50 @@ namespace AddressBook
 {
     internal class addressbook
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int Zip { get; set; }
-        public long Phonenumber { get; set; }
-        public string email { get; set; }
+
+        List<Contact> con = new List<Contact>();
+        public void AddContact()
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Enter Firstname");
+            contact.Firstname = Console.ReadLine()!;
+            Console.WriteLine("Enter Lasttname");
+            contact.Lastname = Console.ReadLine()!;
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine()!;
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine()!;
+            Console.WriteLine("Enter State");
+            contact.State = Console.ReadLine()!;
+            Console.WriteLine("Enter Zip");
+            contact.Zip = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Phonenumber");
+            contact.Phonenumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter email");
+            contact.email = Console.ReadLine()!;
+
+
+            con.Add(contact);
+
+
+        }
+        public void Display()
+        {
+            foreach (var contact in con)
+            {
+                Console.WriteLine("Firstname--" + contact.Firstname);
+                Console.WriteLine("Lastname--" + contact.Lastname);
+                Console.WriteLine("Address--" + contact.Address);
+                Console.WriteLine("City--" + contact.City);
+                Console.WriteLine("State--" + contact.State);
+                Console.WriteLine("Zip--" + contact.Zip);
+                Console.WriteLine("Phonenumber--" + contact.Phonenumber);
+                Console.WriteLine("email--" + contact.email);
+
+            }
+
+        }
+
 
 
     }
