7e59946d4a8635de24e71053d8ccc075049e24f4
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-26 22:13:09 +0530

    Add: edit exiting contact code is added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            addressbook Addressbook = new addressbook();
+            while (true)
+            {
+                Console.WriteLine("select \n1)create contact\n2)display\n3)edit contact");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Addressbook.AddContact();
+                        break;
+                    case 2:
+                        Addressbook.Display();
+                        break;
+                    case 3:
+                        Addressbook.Editcontact();
+                        break;
+                }
+            }
 
         }
     }
